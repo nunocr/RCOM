54cbe35414723a58c82797a1052dc6725d97f4d8
nunocr <nunocr963@gmail.com>
2017-10-16 10:31:51 +0100

    never use gets

--- Aula1/writenoncanonical.c
+++ Aula1/writenoncanonical.c
@@ -25,9 +25,9 @@ int main(int argc, char** argv)
     struct termios oldtio,newtio;
     char buf[255];
     int i, sum = 0, speed = 0;
-    
-    if ( (argc < 2) || 
-  	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
+
+    if ( (argc < 2) ||
+  	     ((strcmp("/dev/ttyS0", argv[1])!=0) &&
   	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
       printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
       exit(1);
@@ -61,9 +61,9 @@ int main(int argc, char** argv)
 
 
 
-  /* 
-    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
-    leitura do(s) próximo(s) caracter(es)
+  /*
+    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
+    leitura do(s) prï¿½ximo(s) caracter(es)
   */
 
 
@@ -78,21 +78,22 @@ int main(int argc, char** argv)
     printf("New termios structure set\n");
 
     //Send
-    printf("Message to send: ");
-    gets(buf);
+    fgets(buf, sizeof(buf), stdin);
+    printf("Message to send: %s\n");
+
 
     res = write(fd, buf, strlen(buf) + 1);
     printf("\n%d bytes written\n", res);
- 
 
-  /* 
-    O ciclo FOR e as instruções seguintes devem ser alterados de modo a respeitar 
-    o indicado no guião 
+
+  /*
+    O ciclo FOR e as instruï¿½ï¿½es seguintes devem ser alterados de modo a respeitar
+    o indicado no guiï¿½o
   */
 
 
 
-   
+
     if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
       perror("tcsetattr");
       exit(-1);
